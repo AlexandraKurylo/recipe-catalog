905b447f3a31627127a81971d782b4b921ed774e
Alexandra <aleksandrakurilo61@gmail.com>
2026-04-21 17:40:05 +0300

    global constants

---
+++ src/constants/global.constants.ts
@@ -0,0 +1,4 @@
+export const API_URL = "https://69e27d793327837a15527075.mockapi.io/api/v1/:recipes";
+
+export const AUTH_STORAGE = "reactCardLogin";
+export const THEME_STORAGE = "reactCardTheme";
