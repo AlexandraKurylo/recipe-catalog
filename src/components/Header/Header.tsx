bfc0f2df43a8b936a47e09069fee5caec851b849
Alexandra <aleksandrakurilo61@gmail.com>
2026-04-21 17:24:22 +0300

    initial commit: recipe catalog

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+import cls from "./Header.module.css";
+
+interface HeaderProps {}
+
+export const Header = ({}: HeaderProps) => <div className={cls.header}>Header Component</div>;
---
+++ src/components/MainLayout/MainLayout.tsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom";
+import cls from "./MainLayout.module.css";
+import { Header } from "../Header";
+
+export const MainLayout = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <>
+      <div className={cls.mainLayout}>
+        <Header />
+        <div className={cls.mainWrapper}>
+          <main className={cls.main}>
+            <Outlet />
+          </main>
+          <footer className={cls.footer}>
+            Portfolio | {currentYear} <br />
+            by Alexandra Kurylo
+          </footer>
+        </div>
+      </div>
+    </>
+  );
+};
